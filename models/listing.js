7da09684c3e5f97084f756c39743dbbda4772189
Adya Kumari <adyajha776@gmail.com>
2024-09-18 20:34:44 +0530

    Add review

--- models/listing.js
+++ models/listing.js
@@ -9,12 +9,19 @@ const listeningSchema =  new Schema({
     description : String,
     Image :{
         type : String,
-        // default:"https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.airbnb.com%2Findia%2Fstays&psig=AOvVaw1YfVKZB1Cnlx1Y6B40O2Rb&ust=1725272025416000&source=images&cd=vfe&opi=89978449&ved=0CBQQjRxqFwoTCMjBtqPBoYgDFQAAAAAdAAAAABAE",
+        default:"https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.airbnb.com%2Findia%2Fstays&psig=AOvVaw1YfVKZB1Cnlx1Y6B40O2Rb&ust=1725272025416000&source=images&cd=vfe&opi=89978449&ved=0CBQQjRxqFwoTCMjBtqPBoYgDFQAAAAAdAAAAABAE",
         set: (v) => v==="" ? "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.airbnb.com%2Findia%2Fstays&psig=AOvVaw1YfVKZB1Cnlx1Y6B40O2Rb&ust=1725272025416000&source=images&cd=vfe&opi=89978449&ved=0CBQQjRxqFwoTCMjBtqPBoYgDFQAAAAAdAAAAABAE " : v,
     }, 
     price : Number,
     location : String,
     country:  String,
+    reviews: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: "Review"
+        },
+    ]
+
 });
 
 const Listing = mongoose.model("Listing",listeningSchema);
