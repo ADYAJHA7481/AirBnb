24251b67aac9ab484e8aa90850e63e9a112a9519
Adya Kumari <adyajha776@gmail.com>
2024-09-25 17:38:37 +0530

    Using Express Route Restructuring Reviews

--- routes/listing.js
+++ routes/listing.js
@@ -1,10 +1,9 @@
 const express =  require("express");
 const router = express.Router();
 const wrapAsync = require("../utils/wrapAsync.js");
-const{listeningSchema, reviewSchema} = require("../schema.js");
+const{listeningSchema} = require("../schema.js");
 const ExpressError = require("../utils/ExpressError.js");
 const Listing = require("../models/listing.js")
-// const Review = require("../models/review.js");
 
 
 const validatelistng = (req, res, next) => {
