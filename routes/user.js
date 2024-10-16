52d340536170274bcbc7d66c7af3f3e1c4a8f86d
Adya Kumari <adyajha776@gmail.com>
2024-10-16 15:12:49 +0530

    post login page with middleware and error handling

--- routes/user.js
+++ routes/user.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const User = require("../models/user.js");
 const wrapasync = require("../utils/wrapAsync.js");
 const passport = require("passport");
+const { saveRedirectUrl } = require("../middleware.js");
 
 router.get("/signup", (req, res) => {
     res.render("users/signup.ejs");
@@ -31,11 +32,12 @@ router.get("/login", (req, res) => {
     res.render("users/login.ejs");
 });
 
-router.post("/login", passport.authenticate("local", {
+router.post("/login",saveRedirectUrl, passport.authenticate("local", {
     failureRedirect: "/login", failureFlash: true,
     }), async (req, res) => {
         req.flash("success","Welcome back to Wonderlust!");
-        res.redirect("/listings");
+        let redirectUrl = res.locals.redirectUrl || "/listings";
+        res.redirect(redirectUrl);
 });
 
 router.get("/logout", (req, res, next) => {
