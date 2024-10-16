414916d0e275a2bafe9e8c23447fb2d4cd1efd5d
Adya Kumari <adyajha776@gmail.com>
2024-10-16 10:29:34 +0530

    Error Handling in User.js

--- routes/user.js
+++ routes/user.js
@@ -1,18 +1,24 @@
 const express =  require("express");
 const router = express.Router();
-const User = require("../models/user.js")
+const User = require("../models/user.js");
+const wrapasync = require("../utils/wrapAsync.js");
 
 router.get("/signup", (req, res) => {
     res.render("users/signup.ejs");
 });
 
-router.post("/signup", async (req, res) => {
-    let {username, email, password} = req.body;
-    const newUser = new User({email, username});
-    const registerdUser = await User.register(newUser,"hello");
-    // console.log(registerdUser);
-    req.flash("success", "Welcome to Wonderlust");
-    res.redirect("/listings");
-});
+router.post("/signup", wrapasync(async(req, res) => {
+    try{
+        let {username, email, password} = req.body;
+        const newUser = new User({email, username});
+        const registerdUser = await User.register(newUser,"hello");
+        // console.log(registerdUser);
+        req.flash("success", "Welcome to Wonderlust");
+        res.redirect("/listings");
+    } catch(e) {
+        req.flash("error", e.message);
+        res.redirect("/signup");
+    }
+}));
 
 module.exports = router;
