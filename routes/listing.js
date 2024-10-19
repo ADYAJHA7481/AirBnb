cda51a553440723c24577ecaf3e0568c7c257eed
Adya Kumari <adyajha776@gmail.com>
2024-10-19 12:57:37 +0530

    MVC  for Listings , Reviews and Users

--- routes/listing.js
+++ routes/listing.js
@@ -4,82 +4,28 @@ const wrapAsync = require("../utils/wrapAsync.js");
 const Listing = require("../models/listing.js")
 const {isLoggedIn, isOwner, validatelisting} = require("../middleware.js");
 
+const listingController = require("../controllers/listings.js");
 
 //Index Route
-router.get("/", wrapAsync( async (req,res)=>{
-    const allListings = await Listing.find({});
-    res.render("listings/index.ejs",{allListings});
-}));
+router.get("/", wrapAsync(listingController.index));
 
 //New Route
-router.get("/new",isLoggedIn,(req,res)=> {
-    res.render("listings/new.ejs")
-});
+router.get("/new",isLoggedIn, listingController.renderNewform);
 
 //Show Route
-router.get("/:id", wrapAsync(async(req,res)=>{
-    let {id} = req.params;
-    const listing = await Listing.findById(id)
-    .populate({
-        path: "reviews", 
-        populate: {
-        path: "author",
-    },
-    })
-    .populate("owner");
-    if(!listing) {
-        req.flash("error", "Listing you requested for does not exist!");
-        res.redirect("/listings");
-    }
-    // console.log(listing);
-    res.render("Listings/show.ejs",{listing});
-}));
+router.get("/:id", wrapAsync(listingController.showListing));
 
 //Create Route
-router.post("/",validatelisting, isLoggedIn, wrapAsync(async (req, res, next)=>
-    {
-        // if(!req.body.listing){
-        //     throw new ExpressError(400, "Send valid data for listing");
-        // }
-        const newListing = new Listing(req.body.listing);
-        // console.log(req.user);
-        newListing.owner = req.user._id;
-        await newListing.save();
-        req.flash("success", "New Listing Created Successfully!!");
-        res.redirect("/listings");
-    }
-));
+router.post("/",validatelisting, isLoggedIn, wrapAsync(listingController.createListing));
 
 //Edit Route
-router.get("/:id/edit",isLoggedIn, isOwner, wrapAsync(async (req,res)=>{
-    let {id} = req.params;
-    const listing = await Listing.findById(id);
-    if(!listing) {
-        req.flash("error", "Listing you requested for does not exist!");
-        res.redirect("/listings");
-    }
-    res.render("listings/edit.ejs",{listing});
-}));
+router.get("/:id/edit",isLoggedIn, isOwner, wrapAsync(listingController.renderEditForm));
 
 //Update Route
-router.put("/:id",validatelisting,isLoggedIn, isOwner, wrapAsync(async (req,res)=>{
-    // if(!req.body.listing){
-    //     throw new ExpressError(404, "Send valid data for listing");
-    // }
-    let {id} = req.params;
-    await Listing.findByIdAndUpdate(id,{...req.body.listing});
-    req.flash("success", "Listing Updated!");
-    res.redirect(`/listings/${id}`);
-}));
+router.put("/:id",validatelisting,isLoggedIn, isOwner, wrapAsync(listingController.updateListing));
 
 //Delete Route
-router.delete("/:id", isLoggedIn, isOwner, wrapAsync(async (req,res)=>{
-    let {id} = req.params;
-    let deletedListing = await Listing.findByIdAndDelete(id);
-    console.log(deletedListing);
-    req.flash("success", "Listing Deleted!");
-    res.redirect(`/listings`);
-}));
+router.delete("/:id", isLoggedIn, isOwner, wrapAsync(listingController.distroyListing));
 
 
 module.exports = router;
--- routes/user.js
+++ routes/user.js
@@ -5,49 +5,18 @@ const wrapasync = require("../utils/wrapAsync.js");
 const passport = require("passport");
 const { saveRedirectUrl } = require("../middleware.js");
 
-router.get("/signup", (req, res) => {
-    res.render("users/signup.ejs");
-});
+const userController = require("../controllers/users.js");
 
-router.post("/signup", wrapasync(async(req, res) => {
-    try{
-        let {username, email, password} = req.body;
-        const newUser = new User({email, username});
-        const registerdUser = await User.register(newUser, password);
-        req.login(registerdUser,(err) => {
-            if(err) {
-               return next(err);
-            }
-            req.flash("success", "Welcome to Wonderlust");
-        res.redirect("/listings");
-        })
-        // console.log(registerdUser);
-    } catch(e) {
-        req.flash("error", e.message);
-        res.redirect("/signup");
-    }
-}));
+router.get("/signup", userController.renderSignupForm);
 
-router.get("/login", (req, res) => {
-    res.render("users/login.ejs");
-});
+router.post("/signup", wrapasync(userController.signup));
+
+router.get("/login", userController.renderLoginForm);
 
 router.post("/login",saveRedirectUrl, passport.authenticate("local", {
     failureRedirect: "/login", failureFlash: true,
-    }), async (req, res) => {
-        req.flash("success","Welcome back to Wonderlust!");
-        let redirectUrl = res.locals.redirectUrl || "/listings";
-        res.redirect(redirectUrl);
-});
+    }), userController.login);
 
-router.get("/logout", (req, res, next) => {
-    req.logout((err) => {
-        if(err) {
-           return next(err);
-        }
-        req.flash("success", "you are logged out!");
-        res.redirect("/listings");
-    })
-});
+router.get("/logout", userController.logout);
 
 module.exports = router;
