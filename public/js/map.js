4bc724776eb44adc1e496397f7742e19baa2cf8a
Adya Kumari <adyajha776@gmail.com>
2024-11-23 10:29:57 +0530

    Map marker to Point Place

--- public/js/map.js
+++ public/js/map.js
@@ -1,7 +1,13 @@
 mapboxgl.accessToken = mapToken;
-    const map = new mapboxgl.Map({
-        container: 'map', // container ID
-        center: [77.209, 28.6139], // starting position [lng, lat]. Note that lat must be set between -90 and 90
-        zoom: 9 // starting zoom
-    });
+
+const map = new mapboxgl.Map({
+    container: 'map', // container ID
+    style: "mapbox://styles/mapbox/streets-v12",
+    center: coordinates, // starting position [lng, lat]. Note that lat must be set between -90 and 90
+    zoom: 10 // starting zoom
+});
+
+const marker = new mapboxgl.Marker({ color: "red"})
+.setLngLat(coordinates)
+.addTo(map);
 	
