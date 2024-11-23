7df49edb0472346d2f13d47940f5a92570c86738
Adya Kumari <adyajha776@gmail.com>
2024-11-23 11:23:13 +0530

    Fixing Home Page

--- app.js
+++ app.js
@@ -52,9 +52,9 @@ const sessionOption = {
     }
 };
 
-app.get("/",(req,res)=>{
-    res.send("Hii");
-})
+// app.get("/",(req,res)=>{
+//     res.send("Hii");
+// })
 
 app.use(session(sessionOption));
 app.use(flash());
